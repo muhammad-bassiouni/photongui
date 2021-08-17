acc355662c2c4b89ce29554dddc746b06ccc7295
Mohamed Ashraf <mohamed501258@gmail.com>
2021-08-17 19:57:46 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,21 +2,32 @@ import pathlib
 from setuptools import setup
 
 
-HERE = pathlib.Path(__file__).parent
+CURRENT = pathlib.Path(__file__).parent
 
 
-README = (HERE / "README.md").read_text()
+README = (CURRENT / "README.md").read_text()
 
+
+
+install_requires = [
+    'pyobjc-core ; sys_platform == "darwin"',
+    'pyobjc-framework-Cocoa ; sys_platform == "darwin"',
+    'pyobjc-framework-WebKit ; sys_platform == "darwin"',
+]
+
+extra_require = {
+    'cef': ['cefpython3'],
+}
 
 setup(
     name="photongui",
     version="1.0.1",
+    author="Muhammed Bassiouni",
+    author_email="",
     description="Build GUI in Python with JavaScript, HTML, and CSS.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/Mohamed501258/photongui",
-    author="Muhammed Bassiouni",
-    author_email="",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -28,7 +39,9 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     include_package_data=True,
+    package_dir={'photongui': 'photongui'},
     packages=['photongui', 'photongui.api'],
     package_data={'photongui': ['photongui/gui/images/icon.png']},
-    install_requires=["cefpython3", "AppKit", "pyobjc"],
+    install_requires=install_requires,
+    extra_requir=extra_require
 )
