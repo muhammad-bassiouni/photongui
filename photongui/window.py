3527c92fcb2f2fc04df656da7400e09c23e858a7
Mohamed Ashraf <mohamed501258@gmail.com>
2021-08-17 22:49:34 +0200

    Update window.py

--- photongui/window.py
+++ photongui/window.py
@@ -3,8 +3,6 @@ import tkinter as tk
 from photongui.windowBlueprint import windowFrame
 
 
-
-
 class createWindow(windowFrame):
     def __init__(self, window_settings=None, parent=None, modal=None, confirm_close=None): 
         self.parent = parent.window if parent else None
